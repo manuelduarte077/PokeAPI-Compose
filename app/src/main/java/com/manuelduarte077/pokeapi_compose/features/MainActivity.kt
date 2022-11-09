c0bf4ee1b64c48399d26675a6fefc38d8ec71f3e
Manuel Duarte  <carlosd99m@gmail.com>
2022-11-09 12:15:01 -0600

    feat: show list pokemon

--- app/src/main/java/com/manuelduarte077/pokeapi_compose/features/MainActivity.kt
+++ app/src/main/java/com/manuelduarte077/pokeapi_compose/features/MainActivity.kt
@@ -3,15 +3,15 @@ package com.manuelduarte077.pokeapi_compose.features
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.manuelduarte077.pokeapi_compose.ui.theme.PokeAPIComposeTheme
+import com.manuelduarte077.pokeapi_compose.viewModel.PokemonListViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,11 +31,30 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PokemonList() {
-    Column {
-        Text(text = "Hello")
+
+    val viewModel = PokemonListViewModel()
+
+    Scaffold(
+        topBar = {
+            MediumTopAppBar(
+                title = {
+                    Text(text = "Pokémon list")
+                }
+            )
+        }
+    ) { padding ->
+        LazyColumn(contentPadding = padding) {
+            items(viewModel.pokemonList) { pokemon ->
+                PokemonCard(pokemon)
+            }
+        }
+
     }
+
+
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/manuelduarte077/pokeapi_compose/features/PokemonCard.kt
+++ app/src/main/java/com/manuelduarte077/pokeapi_compose/features/PokemonCard.kt
@@ -1,4 +1,53 @@
 package com.manuelduarte077.pokeapi_compose.features
 
-class PokemonCard {
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.manuelduarte077.pokeapi_compose.model.Pokemon
+
+
+@Composable
+fun PokemonCard(pokemon: Pokemon) {
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.padding(8.dp)
+    ) {
+        AsyncImage(
+            model = pokemon.imageUrl,
+            "Pokemon image",
+            modifier = Modifier
+                .size(60.dp)
+                .clip(CircleShape)
+                .background(Color.Gray.copy(alpha = 0.1f))
+                .padding(8.dp)
+        )
+
+        Text(
+            "#${pokemon.id}",
+            style = MaterialTheme.typography.headlineMedium,
+            fontWeight = FontWeight.Medium,
+            modifier = Modifier.padding(4.dp)
+        )
+
+        Text(
+            pokemon.name.replaceFirstChar { it.uppercase() },
+            style = MaterialTheme.typography.headlineSmall,
+            fontWeight = FontWeight.Normal
+        )
+        Spacer(modifier = Modifier.fillMaxWidth())
+
+    }
+
+
 }
