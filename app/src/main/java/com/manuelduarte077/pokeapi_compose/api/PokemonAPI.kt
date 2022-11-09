6947fb135f35ff66833b12122700755cbf080122
Manuel Duarte  <carlosd99m@gmail.com>
2022-11-09 11:02:10 -0600

    feat: connection to API endpoints

--- app/src/main/java/com/manuelduarte077/pokeapi_compose/api/PokemonAPI.kt
+++ app/src/main/java/com/manuelduarte077/pokeapi_compose/api/PokemonAPI.kt
@@ -1,5 +1,38 @@
 package com.manuelduarte077.pokeapi_compose.api
 
-class PokemonAPI {
+import com.manuelduarte077.pokeapi_compose.model.Pokemon
+import com.manuelduarte077.pokeapi_compose.model.PokemonList
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+object PokemonAPI {
+    
+    interface PokemonAPI {
+        @GET("pokemon?limit=151")
+        fun loadPokemon(): Call<PokemonList>
+    }
+
+    fun loadPokemon(success: (pokemonList: List<Pokemon>) -> Unit, failure: () -> Unit) {
+        val retrofit = Retrofit.Builder().baseUrl("https://pokeapi.co/api/v2/")
+            .addConverterFactory(GsonConverterFactory.create()).build()
+
+        val  service = retrofit.create(PokemonAPI::class.java)
+
+        service.loadPokemon().enqueue(object: Callback<PokemonList> {
+
+            override fun onResponse(call: Call<PokemonList>, response: Response<PokemonList>) {
+                success(response.body()?.results ?: listOf())
+            }
+
+            override fun onFailure(call: Call<PokemonList>, t: Throwable) {
+                failure()
+            }
+        })
+
+    }
 
 }
--- app/src/main/java/com/manuelduarte077/pokeapi_compose/model/Pokemon.kt
+++ app/src/main/java/com/manuelduarte077/pokeapi_compose/model/Pokemon.kt
@@ -1,2 +1,17 @@
 package com.manuelduarte077.pokeapi_compose.model
 
+
+data class PokemonList(val results: List<Pokemon>)
+
+data class Pokemon(val name: String, val url: String) {
+
+    val id: Int
+        get() {
+            val components = url.split("/")
+            return components[components.size - 2].toIntOrNull() ?: 0
+        }
+
+    val imageUrl: String
+        get() = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/$id.png"
+
+}
